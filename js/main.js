eb9a1283340caad5e2e55bc1a0249b683850b33a
albondad <36680782+albondad@users.noreply.github.com>
2019-10-25 16:48:32 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 //VARIABLES
 var channels = [];
-var searchFilter = "channelName"
+var searchFilter = "id"
 
 
 //CHANNEL CLASS
@@ -56,8 +56,8 @@ class Channel {
                 datasets: [{
                         data: this.subscriberCounts,
                         label: "Subscribers",
-                        borderColor: "#CC6666",
-                        pointBackgroundColor: "#CC6666",
+                        borderColor: "#E84747",
+                        pointBackgroundColor: "#E84747",
                         pointBorderWidth: 5,
                         lineTension: 0,
                         fill: true
@@ -74,36 +74,36 @@ function displayStatistics(channel) {
         '<div class="container-fluid w-75">' +
             '<!--header-->' +
             '<div class="row bg-red01">' +
-                '<div class="col-md-12 col-lg-6 foSize-2em foColor-white01 text-center text-lg-left foWeight-700 pt-2 pb-2">' + channel.username.toUpperCase() + '</div>' +
-                '<div class="col-md-12 col-lg-6 foSize-2em foColor-white01 text-center text-lg-right foWeight-100 pt-2 pb-2"><a class="" href="https://www.youtube.com/channel/' + channel.id + '" target="_blank">View Channel</a></div>' +
+                '<div class="col-md-12 col-lg-6 foSize-2em foColor-black01 text-center text-lg-left foWeight-700 pt-2 pb-2">' + channel.username.toUpperCase() + '</div>' +
+                '<div class="col-md-12 col-lg-6 foSize-2em foColor-black01 text-center text-lg-right foWeight-100 pt-2 pb-2"><a class="" href="https://www.youtube.com/channel/' + channel.id + '" target="_blank">View Channel</a></div>' +
             '</div>' +
             '<!--statistics-->' +
-            '<div class="row bg-gray02 pt-2">' +
-                '<div class="statsContentContainer col-sm-12 col-md-12 col-lg-4 d-flex justify-content-center align-items-center foColor-white01 pt-5 text-center">' +
+            '<div class="row bg-blac01 p-5 statsContainer">' +
+                '<div class="statsContentContainer col-sm-12 col-md-12 col-lg-4 d-flex justify-content-center align-items-center foColor-red01 pt-5 text-center">' +
                     '<p>' +
                         '<span class="statsLabel">SUBSCRIBERS</span>' +
                         '<br/>' +
                         '<span id="' + channel.username + '_currentSubscriberCount' + '" class="statsContent01">' + channel.currentSubscriberCount.toLocaleString('en') + '</span>' +
                     '</p>' +
                 '</div>' +
-                '<div class="col-sm-12 col-md-12 col-lg-8 d-flex justify-content-center align-items-start foSize-2em foColor-white01">' +
+                '<div class="col-sm-12 col-md-12 col-lg-8 d-flex justify-content-center align-items-start foSize-2em foColor-red01">' +
                     '<canvas id="' + channel.username + '_subscriberChart' + '""></canvas>' +
                 '</div>' +
-                '<div class="statsContentContainer col-sm-12 col-md-4 col-lg-4 d-flex justify-content-center align-items-start foColor-white01 pt-5 text-center">' +
+                '<div class="statsContentContainer col-sm-12 col-md-4 col-lg-4 d-flex justify-content-center align-items-start foColor-red01 pt-5 text-center">' +
                     '<p>' +
                         '<span class="statsLabel">DESCRIPTION</span>' +
                         '<br/>' +
                         '<span class="' + channel.descriptionContentType + '">' + channel.description + '</span>' +
                     '</p>' +
                 '</div>' +
-                '<div class="statsContentContainer col-sm-12 col-md-4 col-lg-4 d-flex justify-content-center align-items-start foColor-white01 pt-5 text-center">' +
+                '<div class="statsContentContainer col-sm-12 col-md-4 col-lg-4 d-flex justify-content-center align-items-start foColor-red01 pt-5 text-center">' +
                     '<p>' +
                         '<span class="statsLabel">DESCRIPTION</span>' +
                         '<br/>' +
                         '<span class="statsContent01">' + channel.country + '</span>' +
                     '</p>' +
                 '</div>' +
-                '<div class="statsContentContainer col-sm-12 col-md-4 col-lg-4 d-flex justify-content-center align-items-start foColor-white01 pt-5 text-center">' +
+                '<div class="statsContentContainer col-sm-12 col-md-4 col-lg-4 d-flex justify-content-center align-items-start foColor-red01 pt-5 text-center">' +
                     '<p>' +
                         '<span class="statsLabel">TOTAL VIEWS</span>' +
                         '<br/>' +
@@ -132,7 +132,22 @@ function loadClient() {
                 $("#loading").html("There was an error loading please try again.")
             });
 }
+
 function addChannelBy(searchFilter, input) {
+    //searches for valid id in url
+    if (searchFilter == "id" ){
+        //trying to isolate id or channel name
+        input = input.replace("https://www.youtube.com/channel/", '');
+        input = input.replace("www.youtube.com/channel/", '');
+        input = input.replace("https://www.youtube.com/user/", '');
+        input = input.replace("www.youtube.com/user/", '');
+        input = input.slice(0, 24);
+        if (input.length < 24) {
+            searchFilter = "channelName"
+        }
+    }
+
+    //searching by channel name
     if (searchFilter == "channelName") {
         //checking for duplicates
         isDuplicate = false
@@ -141,7 +156,6 @@ function addChannelBy(searchFilter, input) {
                 isDuplicate = true;
             }
         }
-
         //adding channel
         if (!isDuplicate) {
             gapi.client.youtube.channels.list({
@@ -154,13 +168,16 @@ function addChannelBy(searchFilter, input) {
                     displayStatistics(channels[channels.length-1]);
                     isDuplicate = false;
                     $("#errors").html("");
+                    console.log(channels[channels.length - 1]);
                 }
                 else {
-                    $("#errors").html("The channel wasn't found.");
+                    $("#errors").html("the channel wasn't found");
                 }
             });
         }
     }
+
+    //searching by channel url
     if (searchFilter == "id") {
         //checking for duplicates
         isDuplicate = false
@@ -169,7 +186,6 @@ function addChannelBy(searchFilter, input) {
                 isDuplicate = true;
             }
         }
-
         //adding channel
         if (!isDuplicate) {
             gapi.client.youtube.channels.list({
@@ -181,6 +197,7 @@ function addChannelBy(searchFilter, input) {
                     channels.push(new Channel(newStatistics));
                     displayStatistics(channels[channels.length-1]);
                     isDuplicate = false;
+                    console.log(channels[channels.length - 1]);
                 }
                 else {
                     console.log("channel is undefined")
@@ -189,6 +206,7 @@ function addChannelBy(searchFilter, input) {
         }
     }
 }
+
 gapi.load("client", loadClient);
 
 //AFTER DOCUMENT LOADS
@@ -201,15 +219,22 @@ $(function() {
             addChannelBy("id", $("#inp_search").val());
         }
     });
+    $("#lnk_byChannelURL").click(function() {
+        $("#btn_filter").text("Search By URL");
+        $("#inp_search").attr("placeholder", "channel URL");
+        searchFilter = "id"
+    });
     $("#lnk_byChannelName").click(function() {
+        $("#btn_filter").text("Search By Name");
         $("#inp_search").attr("placeholder", "channel name");
         searchFilter = "channelName"
     });
     $("#lnk_byChannelID").click(function() {
-        $("#inp_search").attr("placeholder", "channel id");
+        $("#btn_filter").text("Search By ID");
+        $("#inp_search").attr("placeholder", "channel ID");
         searchFilter = "id"
     });
-
+    //updating charts every 10 seconds
     setInterval(function() {
         for(i=0; i<channels.length; i++) {
             gapi.client.youtube.channels.list({
